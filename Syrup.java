e12e1adda7a1504bf5e2bc86d1b89f50f27d935c
Akshay1438 <63441844+Akshay1438@users.noreply.github.com>
2021-02-18 23:12:46 +0530

    Add files via upload
    Assignment3Problem5

---
+++ Syrup.java
@@ -0,0 +1,12 @@
+package assignment3Problem5;
+
+
+public class Syrup extends Medicine {
+
+	void displayLabel() {
+		System.out.println("SYRUP:");
+		System.out.println("Keep out of reach of Children");
+
+	}
+
+}
---
+++ Tablet.java
@@ -0,0 +1,12 @@
+package assignment3Problem5;
+
+public class Tablet extends Medicine {
+
+	@Override
+	void displayLabel() {
+		System.out.println("TABLET:");
+		System.out.println("Store in a cool dry place");
+
+	}
+
+}
